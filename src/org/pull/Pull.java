766c0cda669a836aaa3004f400279f0008889767
mathisankar14 <smathisankar@gmail.com>
2022-10-30 16:36:35 +0530

    master task

---
+++ src/org/pull/Pull.java
@@ -0,0 +1,18 @@
+package org.pull;
+
+public class Pull {
+	private void empName() {
+		System.out.println("EmpNmae: Mathi");
+	}
+
+	private void empId() {
+		System.out.println("EmpId:2563");
+	}
+
+	public static void main(String[] args) {
+		Pull p = new Pull();
+		p.empName();
+		p.empId();
+	}
+
+}
