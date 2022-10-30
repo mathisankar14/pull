ffab33b5be9a62138ed99f3f02b918546191ac02
mathisankar14 <smathisankar@gmail.com>
2022-10-30 17:03:03 +0530

    mathi task

--- src/org/pull/Pull.java
+++ src/org/pull/Pull.java
@@ -8,11 +8,15 @@ public class Pull {
 	private void empId() {
 		System.out.println("EmpId:2563");
 	}
+	private void empPhone() {
+		System.out.println("EmpPh:7904523647");
+	}
 
 	public static void main(String[] args) {
 		Pull p = new Pull();
 		p.empName();
 		p.empId();
+		p.empPhone();
 	}
 
 }
